b8b195b306d067ac1b857e0c3f2924b1637859a2
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2024-04-02 21:11:54 -0400

    Update 11ty directories, primarily to output to dist

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,13 @@
 const pluginWebC = require("@11ty/eleventy-plugin-webc");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginWebC);
+
+  return {
+    dir: {
+      includes: "../_includes",
+      input: "src",
+      output: "dist",
+    },
+  };
 };
