615a560f01831e8572ef9b232519670c97c3a862
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2024-04-11 21:33:09 -0400

    Replace WebC with Liquid for consistency

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,4 @@
-const pluginWebC = require("@11ty/eleventy-plugin-webc");
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(pluginWebC);
-
   return {
     dir: {
       data: "../_data",
