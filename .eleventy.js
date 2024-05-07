efddaedf7a5fff63c3cff539e62020cfd401acf7
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2024-05-06 22:42:50 -0400

    Add back-to-tiers link component and put it where appropriate

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const pluginWebC = require("@11ty/eleventy-plugin-webc");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addPlugin(pluginWebC);
+
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("fonts");
 
