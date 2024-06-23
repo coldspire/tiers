34fb041688b463ae07eae3a91bc88a674f57fb5b
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2024-06-22 22:46:21 -0400

    Add Markdown footnote support and other goodies

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const pluginWebC = require("@11ty/eleventy-plugin-webc");
+const markdownIt = require("markdown-it"); // Included as part of 11ty installation
+const markdownItFootnote = require("markdown-it-footnote");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
@@ -7,6 +9,16 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("fonts");
+
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt({
+      html: true, // Enable HTML tags in source
+      breaks: true, // Convert '\n' in paragraphs into <br>
+      linkify: true, // Autoconvert URL-like text to links,
+      typographer: true, // Enable some language-neutral replacement + quotes beautification
+    }).use(markdownItFootnote),
+  );
 
   return {
     dir: {
