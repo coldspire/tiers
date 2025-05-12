2b105ccaf756da058ceaeae9499de0c942f8d282
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2025-05-11 22:03:42 -0400

    Finish conversion to JS tiers

---
+++ _data/tiers/chris-nolan-movies.js
@@ -0,0 +1,47 @@
+const { RankIndex } = require("../../data-helpers/rank-index.js");
+
+module.exports = {
+  tiersTitle: "Christopher Nolan Movies",
+  tiersId: "chris-nolan-movies",
+  aboutText: `Good job, Chris`,
+  ranks: [
+    {
+      rankName: "S",
+      entries: [
+        {
+          id: "memento",
+          title: "Memento",
+        },
+      ],
+    },
+    {
+      rankName: "A",
+      entries: [],
+    },
+    {
+      rankName: "B",
+      entries: [
+        {
+          id: "tenet",
+          title: "Tenet",
+        },
+      ],
+    },
+    {
+      rankName: "C",
+      entries: [],
+    },
+    {
+      rankName: "D",
+      entries: [],
+    },
+    {
+      rankName: "F",
+      entries: [],
+    },
+    {
+      rankName: "O",
+      entries: [],
+    },
+  ],
+};
---
+++ _data/tiers/kids-shows.js
@@ -0,0 +1,47 @@
+const { RankIndex } = require("../../data-helpers/rank-index.js");
+
+module.exports = {
+  tiersTitle: "Kids Shows",
+  tiersId: "kids-shows",
+  aboutText: `Good job, Kids`,
+  ranks: [
+    {
+      rankName: "S",
+      entries: [
+        {
+          id: "bluey",
+          title: "Bluey",
+        },
+      ],
+    },
+    {
+      rankName: "A",
+      entries: [],
+    },
+    {
+      rankName: "B",
+      entries: [
+        {
+          id: "stinky-dirty",
+          title: "Stinky Dirty",
+        },
+      ],
+    },
+    {
+      rankName: "C",
+      entries: [],
+    },
+    {
+      rankName: "D",
+      entries: [],
+    },
+    {
+      rankName: "F",
+      entries: [],
+    },
+    {
+      rankName: "O",
+      entries: [],
+    },
+  ],
+};
