c877aec75e775ba2fea88ce5e833e396f3f0b9ad
Owen <5332788+coldspire@users.noreply.github.com>
2025-05-12 22:16:34 -0400

    Change separate pages to contain in a dialog; use global data from JS files (#27)
    * Add shared dialog to tier layout
    
    * Fix bug with dialog not closing correctly; clear hash on dialog close
    
    * Update about button and dialog for about-tiers stuff
    
    * Fix bug with dialog not closing correctly; clear hash on dialog close
    
    * Update presentation for content that causes scrolling
    
    * Replace description horiz rules with proper styling
    
    * Partial work to change the generation of separate entry pages
    
    * Add eleventy inspect filter
    
    * Finish conversion to JS tiers
    
    * Remove unused RankIndex
    
    * Add output of text for entries

---
+++ _data/tiers/chris-nolan-movies.js
@@ -0,0 +1,46 @@
+module.exports = {
+  tiersTitle: "Christopher Nolan Movies",
+  tiersId: "chris-nolan-movies",
+  aboutText: `Good job, Chris`,
+  ranks: [
+    {
+      rankName: "S",
+      entries: [
+        {
+          id: "memento",
+          title: "Memento",
+          text: `Placeholder text!`,
+        },
+      ],
+    },
+    {
+      rankName: "A",
+      entries: [],
+    },
+    {
+      rankName: "B",
+      entries: [
+        {
+          id: "tenet",
+          title: "Tenet",
+        },
+      ],
+    },
+    {
+      rankName: "C",
+      entries: [],
+    },
+    {
+      rankName: "D",
+      entries: [],
+    },
+    {
+      rankName: "F",
+      entries: [],
+    },
+    {
+      rankName: "O",
+      entries: [],
+    },
+  ],
+};
--- src/tiers/kids-shows/_about.md
+++
@@ -1,6 +0,0 @@
----
-layout: tiers-about.liquid
----
-
-TODO KIDS
-
