6c119cc0adc126bd6557167a254a9dbbd1391b1c
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2025-12-21 17:49:59 -0500

    All entries and some write-ups

---
+++ _data/tiers/tesd-xmas.js
@@ -0,0 +1,115 @@
+export default {
+  tiersTitle: "Tell 'Em Steve-Dave! Christmas Specials",
+  tiersId: "tesd-xmas",
+  aboutText: `
+    _Tell 'Em Steve-Dave!_ is the only podcast I've listened to since it's inception. Since the podcast began back in 2011, that's a _lot_ of years of TESD listening. The pod puts out an annual Christmas special XXXXXXXXXXXXXX
+  `,
+  ranks: [
+    {
+      rankName: "S",
+      entries: [
+        {
+          id: "2014",
+          title: "2014",
+          text: `Can't beat a series of games where Sal lifts Bry and Q to new levels of comedic competition, and the Ming's underdog team utterly floundering and underperforming from the basement, resulting in the losing-team's leader receiving a TESD tattoo in the ultra-satisfying conclusion. Tension-free due to the podcast album art featuring the TESD tattoo prominently, making the conclusion known before playing the first second.`,
+        },
+      ],
+    },
+    {
+      rankName: "A",
+      entries: [
+        {
+          id: "2019",
+          title: "2019",
+          text: `This is the only Christmas special I have in video form, where the crew podcasts from what looks like a music studio located in a basement that was last interior-designed in the 80s. The location somehow fits TESD's traditional misfit patchwork success.
+          
+          The banter is great, and the games are varied and competitive. Highlights include Bryan's, er, generous gift-giving and the motley combination of Sunday Jeff and Git-em stumbling to victory.`,
+        },
+        {
+          id: "2021",
+          title: "2021",
+          text: `XXXXXXXXXXXXXXX
+          
+          The big minus for this episode is the reduction of Bry, simply due to the design of the show where each of the numerous participants have limited speaking times.`,
+        },
+      ],
+    },
+    {
+      rankName: "B",
+      entries: [
+        {
+          id: "2011",
+          title: "2011",
+          text: `A bit threadbare, but this inaugural special has a warmth that the subsequent episodes never really match. Fewer games and more friendly chat is probably a big contributor to the cozy feeling.`,
+        },
+        {
+          id: "2012",
+          title: "2012",
+        },
+        {
+          id: "2013",
+          title: "2013",
+          text: `Overstuffed but _dang_ if the crew isn't trying their darndest to put on a big show. The TESD Krampuses is the highlight; the radio play, while really well produced, is too mean-spirited to be funny. But that was probably the point.
+          
+          I actually prefer the extra Declan-ordered bonus content to the official special, since the bonus content is just the core crew of three chatting through Christmas scenarios and traditions.`,
+        },
+        {
+          id: "2015",
+          title: "2015",
+          text: `The crew takes a swerve and holds a Christmas special all about Christ, or at least that's the idea. It takes a bit for everyone to get on board, but XXXXXXXXXXXXXX`,
+        },
+        {
+          id: "2020",
+          title: "2020",
+          text: `Tom reigns with a massive selection of party games, which allows Walt to bring his cranky competitive side to the banter.`,
+        },
+      ],
+    },
+    {
+      rankName: "C",
+      entries: [
+        {
+          id: "2016",
+          title: "2016",
+        },
+        {
+          id: "2022",
+          title: "2022",
+          text: `When I think of this special, I can only remember two things about it: it has a Frank 5 focus, and Walt says "Don't lick the donkey!" in response to a teammate's marshmallow-mouthed game hint, which is a very funny thing to say. 
+          
+          Everything else, the 99% rest, is fairly typical TESD banter and games. Fine, sure, but it lacks the Christmas Special charm and elevation.`,
+        },
+        {
+          id: "2023",
+          title: "2023",
+          text: `The setting of a white-elephant-like distribution of participants to play games doesn't pay off much, except for the opportunity to disallow Jimmy the Hair Guy a part in the special for a second year running. Other than that, it's Christmas-based games as usual, with few stand-out moments.`,
+        },
+      ],
+    },
+    {
+      rankName: "D",
+      entries: [],
+    },
+    {
+      rankName: "F",
+      entries: [],
+    },
+    {
+      rankName: "O",
+      entries: [
+        {
+          id: "2017",
+          title: "2017",
+        },
+        {
+          id: "2018",
+          title: "2018",
+        },
+        {
+          id: "2024",
+          title: "2024",
+        },
+      ],
+    },
+  ],
+};
