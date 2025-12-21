5d2a07ee12a3e21a1225921751a5b72a8f3fd6fd
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2025-12-20 22:35:38 -0500

    All entries and some write-ups

---
+++ _data/tiers/christmas-movies.js
@@ -0,0 +1,108 @@
+export default {
+  tiersTitle: "Christmas Movies",
+  tiersId: "christmas-movies",
+  aboutText: `
+  For every December since, like, 2016 (and this is written in December 2025), on Christmas Eve _Eve_ Melissa and I buy sushi and Chinese food and watch a Christmas movie. Sometimes we watch a new movie to both us, sometimes new to one of us, or an old pick for both of us. Regardless of the pick, the Eve Eve movie and meal is something I look forward to every year. 
+  
+  These tiers track the movies we've seen every since the first year and is updated annually.
+  `,
+  ranks: [
+    {
+      rankName: "S",
+      entries: [
+        {
+          id: "arthur-christmas",
+          title: "Arthur Christmas",
+        },
+      ],
+    },
+    {
+      rankName: "A",
+      entries: [
+        {
+          id: "the-muppet-christmas-carol",
+          title: "The Muppet Christmas Carol",
+        },
+      ],
+    },
+    {
+      rankName: "B",
+      entries: [
+        {
+          id: "elf",
+          title: "Elf",
+        },
+        {
+          id: "jingle-all-the-way",
+          title: "Jingle All the Way",
+        },
+      ],
+    },
+    {
+      rankName: "C",
+      entries: [
+        {
+          id: "klaus",
+          title: "Klaus",
+        },
+        {
+          id: "national-lampoons-christmas-vacation",
+          title: "National Lampoon's Christmas Vacation",
+        },
+        {
+          id: "the-santa-clause",
+          title: "The Santa Clause",
+        },
+        {
+          id: "the-santa-clause-2",
+          title: "The Santa Clause 2",
+        },
+        {
+          id: "scrooged",
+          title: "Scrooged",
+        },
+        {
+          id: "the-christmas-chronicles",
+          title: "The Christmas Chronicles",
+        },
+        {
+          id: "white-christmas",
+          title: "White Christmas",
+        },
+      ],
+    },
+    {
+      rankName: "D",
+      entries: [
+        {
+          id: "christmas-in-connecticut",
+          title: "Christmas in Connecticut",
+        },
+        {
+          id: "the-christmas-chronicles-2",
+          title: "The Christmas Chronicles 2",
+        },
+      ],
+    },
+    {
+      rankName: "F",
+      entries: [
+        {
+          id: "the-santa-clause-3",
+          title: "The Santa Clause 3",
+        },
+        {
+          id: "die-hard",
+          title: "Die Hard",
+          text: `An excellent movie, of course. But not a Christmas movie in our house.
+          
+          If the Christmas party setting was replaced with a New Year's Day party, everything else in the movie would stay the same. So while _Die Hard_ is a (again, excellent) movie that takes place around Christmas _time_, it is not a movie that essentially involves Christmas. (And the film distributor apparently felt the same: the movie was released in the summer of 1988.)`,
+        },
+      ],
+    },
+    {
+      rankName: "O",
+      entries: [],
+    },
+  ],
+};
