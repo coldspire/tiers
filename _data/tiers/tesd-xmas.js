afbcee4c6f4de95dde2fcbaef7ecf188e61d0449
Owen Anderson <5332788+coldspire@users.noreply.github.com>
2025-12-26 15:01:24 -0500

    Writing

--- _data/tiers/tesd-xmas.js
+++ _data/tiers/tesd-xmas.js
@@ -11,7 +11,23 @@ export default {
         {
           id: "2014",
           title: "2014",
-          text: `Can't beat a series of games where Sal lifts Bry and Q to new levels of comedic competition, and the Ming's underdog team utterly floundering and underperforming from the basement, resulting in the losing-team's leader receiving a TESD tattoo in the ultra-satisfying conclusion. Tension-free due to the podcast album art featuring the TESD tattoo prominently, making the conclusion known before playing the first second.`,
+          text: `
+          The one where Ming and his team lose at a series of games, resulting in Ming getting a TESD tattoo.
+          
+          Highlights:
+          <ul class="tier-desc__highlights">
+          <li>Sal Vulcano guest-starring and fueling the antics</li>
+          <li>The utter humiliation of the visiting team, who don't garner any points for most of the episode</li>
+          <li>Ming meeting the theme of the rap challenge with a well-spoken friendly cooperative rap, and Bryan coming to the table with a very poorly delivered mean-spirited rap</li>
+          <li>A live tattooing of the losing team leader</li>
+          <li>The display of the tattoo on the podcast art, dispelling from the start any tension of whom the loser will be</li>
+          </ul>
+          
+          Lowlights: 
+          <ul class="tier-desc__lowlights">
+          <li>None!</li>
+          </ul>
+          `,
         },
       ],
     },
@@ -21,16 +37,39 @@ export default {
         {
           id: "2019",
           title: "2019",
-          text: `This is the only Christmas special I have in video form, where the crew podcasts from what looks like a music studio located in a basement that was last interior-designed in the 80s. The location somehow fits TESD's traditional misfit patchwork success.
+          text: `The one where three teams compete to raise money for a "failing orphanage," something that Walt claims to have wanted to do since childhood.
           
-          The banter is great, and the games are varied and competitive. Highlights include Bryan's, er, generous gift-giving and the motley combination of Sunday Jeff and Git-em stumbling to victory.`,
+          Highlights:
+          <ul class="tier-desc__highlights">
+          <li>Bryan is extraordinarily sharp and quick-witted in this one</li>
+          <li>Bryan's playfully offensive gift-giving</li>
+          <li>The games are varied, and the contestants are committed and competitive</li>
+          <li>This is the only special I have video of, and it makes a big difference</li>
+          </ul>
+          
+          Lowlights: 
+          <ul class="tier-desc__lowlights">
+          <li>Gitem chirping up a bit too often</li>
+          </ul>`,
         },
         {
           id: "2021",
           title: "2021",
-          text: `XXXXXXXXXXXXXXX
+          text: `The one where Tim the Record Store Clerk judges everyone else, and Q reveals a new addition to his collection of creepy objects.          
           
-          The big minus for this episode is the reduction of Bry, simply due to the design of the show where each of the numerous participants have limited speaking times.`,
+          Highlights:
+          <ul class="tier-desc__highlights">
+          <li>The format where contestants sound off on a particular Christmas-related topic and Tim picks a winner is fun, engaging, and gives everyone equal time to participate</li>
+          <li>Walt actually gets to participate in the games for once</li>
+          <li>Q showing his dominant prowess for imagination and storytelling after years of impromptu comedy</li>
+          </ul>
+          
+          Lowlights: 
+          <ul class="tier-desc__lowlights">
+          <li>Not enough Bryan, as he doesn't get his usual opportunities due to the format of the episode</li>
+          <li>Q's unveiling of Krumpy, the mummified corpse of one of Santa's elves and his second oddity acquisition after the Prussian Kissing Devil skull, clearly underwhelms his audience and falls flat in the audio-only version (which is the only version I have)</li>
+          </ul>
+          `,
         },
       ],
     },
@@ -40,28 +79,84 @@ export default {
         {
           id: "2011",
           title: "2011",
-          text: `A bit threadbare, but this inaugural special has a warmth that the subsequent episodes never really match. Fewer games and more friendly chat is probably a big contributor to the cozy feeling.`,
+          text: `The inaugural episode, with only Bryan, Walt, Q, Sunday Jeff, and Ming participating.
+          
+          Highlights:
+          <ul class="tier-desc__highlights">
+          <li>Ming's revelation to the group that his parents never celebrated Christmas, and thus he had no Christmas celebrations until he was an adult</li>
+          <li>Jeff's guess for best Christmas movie as "The Wizard of Oz"
+          <li>Walt's gift-giving, particularly his intended heartwarming inspirational "sonata" recording for Bryan that, while completely sincere, comes across as stiff and strange</li>
+          </ul>
+          
+          Lowlights: 
+          <ul class="tier-desc__lowlights">
+          <li>Wish the length could be longer, but, hey, this was the first time</li>
+          </ul>`,
         },
         {
           id: "2012",
           title: "2012",
+          text: `
+          Highlights:
+          <ul class="tier-desc__highlights">
+          <li>The caroling works pretty well, because nobody winks (except Bryan, a bit). Ming and Q's rendition of "Baby It's Cold Outside" is especially good</li>
+          <li>The whole "Christmas Overkill" bit, in which the group (and most listeners, I bet, and me) are introduced to Krampus and Black Pete</li>
+          </ul>
+          
+          Lowlights: 
+          <ul class="tier-desc__lowlights">
+          <li>The offensive Christmas songs discussion is fishing for strong reactions a bit much, especially for "Baby It's Cold Outside"</li>
+          <li>Ming's One-True-Three, while being a good rendition of one of those, is easily skippable on repeat listens</li>
+          </ul>`,
         },
         {
           id: "2013",
           title: "2013",
-          text: `Overstuffed but _dang_ if the crew isn't trying their darndest to put on a big show. The TESD Krampuses is the highlight; the radio play, while really well produced, is too mean-spirited to be funny. But that was probably the point.
-          
-          I actually prefer the extra Declan-ordered bonus content to the official special, since the bonus content is just the core crew of three chatting through Christmas scenarios and traditions.`,
+          text: `
+            The one with the TESD Krampuses and the Ming-antagonist _Christmas Carol_ radio play.
+
+            Highlights:
+            <ul class="tier-desc__highlights">
+              <li>Chock full o' variety. Gitem's memories, TESD Krampuses, radio play!</li>
+              <li>The TESD Krampuses are a hoot and deserved to be featured more in this episode and later ones</li>
+              <li>The radio play is well-produced and -acted, with performances and extra effects really shining</li>
+              <li>The separate bonus content is great, maybe more enjoyable minute-to-minute than the actual special</li>
+            </ul>
+            
+            Lowlights:
+              <ul class="tier-desc__lowlights">
+                <li>The radio play has few good jabs and mostly just comes off as mean-spirited. Which was the point, I'm sure</li>
+                <li>Despite all the stuff packed into the episode, it runs a forced 90 minutes and feels a bit limp</li>
+              </ul>
+          `,
         },
         {
           id: "2015",
           title: "2015",
-          text: `The crew takes a swerve and holds a Christmas special all about Christ, or at least that's the idea. It takes a bit for everyone to get on board, but XXXXXXXXXXXXXX`,
+          text: `
+          Highlights:
+          <ul class="tier-desc__highlights">
+          <li></li>
+          </ul>
+          
+          Lowlights: 
+          <ul class="tier-desc__lowlights">
+          <li></li>
+          </ul>`,
         },
         {
           id: "2020",
           title: "2020",
-          text: `Tom reigns with a massive selection of party games, which allows Walt to bring his cranky competitive side to the banter.`,
+          text: `
+          Highlights:
+          <ul class="tier-desc__highlights">
+          <li></li>
+          </ul>
+          
+          Lowlights: 
+          <ul class="tier-desc__lowlights">
+          <li></li>
+          </ul>`,
         },
       ],
     },
@@ -71,18 +166,49 @@ export default {
         {
           id: "2016",
           title: "2016",
+          text: `
+          Highlights:
+          <ul class="tier-desc__highlights">
+          <li></li>
+          </ul>
+          
+          Lowlights: 
+          <ul class="tier-desc__lowlights">
+          <li></li>
+          </ul>`,
         },
         {
           id: "2022",
           title: "2022",
-          text: `When I think of this special, I can only remember two things about it: it has a Frank 5 focus, and Walt says "Don't lick the donkey!" in response to a teammate's marshmallow-mouthed game hint, which is a very funny thing to say. 
+          text: `The one where Frank 5 is given focus.
+
+          Highlights:
+          <ul class="tier-desc__highlights">
+          <li>The mumbling-through-marshmallows game is pretty good, especially the part where Walt attempts a guess with "Don't lick the donkey"</li>
+          </ul>
           
-          Everything else, the 99% rest, is fairly typical TESD banter and games. Fine, sure, but it lacks the Christmas Special charm and elevation.`,
+          Lowlights: 
+          <ul class="tier-desc__lowlights">
+          <li>Too much Frank 5, who is not sharp, witty, or combative enough to hold up an episode</li>
+          <li>Not particularly many yuks</li>
+          <li>The usual games are getting pretty stale by this point</li>
+          </ul>`,
         },
         {
           id: "2023",
           title: "2023",
-          text: `The setting of a white-elephant-like distribution of participants to play games doesn't pay off much, except for the opportunity to disallow Jimmy the Hair Guy a part in the special for a second year running. Other than that, it's Christmas-based games as usual, with few stand-out moments.`,
+          text: `The one with the white elephant contestant trading.
+
+          Highlights:
+          <ul class="tier-desc__highlights">
+          <li>None, really</li>
+          </ul>
+          
+          Lowlights: 
+          <ul class="tier-desc__lowlights">
+          <li>The white-elephant contestant trading doesn't pay off</li>
+          <li>Fairly limp in terms of banter and games played</li>
+          </ul>`,
         },
       ],
     },
