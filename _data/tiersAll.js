b45955d019607baa8d16a15da21e155d9642e715
Owen <5332788+coldspire@users.noreply.github.com>
2026-01-04 14:50:39 -0500

    Change tiers to be assembled by individual files (#34)
    * Move existing tiers data into _oldsrc/
    
    * Add _data/tiersAll.js to dynamically create tiers data from dirs/files
    
    * Update templates for new tiersAll format
    
    * Convert Chris Nolan entries to new tiersAll format

---
+++ _data/tiersAll.js
@@ -0,0 +1,61 @@
+import { readdir } from "node:fs/promises";
+
+const tiersPathFromWorkspaceRoot = "./tiers/";
+
+async function createTiersFromDir(dir) {
+  const tiersId = dir;
+  const files = await readdir(tiersPathFromWorkspaceRoot + tiersId);
+  let tiers = {
+    id: tiersId,
+    title: "",
+    aboutText: "",
+    ranks: {
+      S: [],
+      A: [],
+      B: [],
+      C: [],
+      D: [],
+      F: [],
+      O: [],
+    },
+  };
+
+  for (const file of files) {
+    const filePath = "../" + tiersPathFromWorkspaceRoot + tiersId + "/" + file;
+    const fileContents = (await import(filePath)).default;
+    if (file === "_about.js") {
+      tiers = {
+        ...tiers,
+        ...fileContents, // The About file contains information about all the tiers, so it's added to the top level properties
+      };
+    } else {
+      const tierEntry = {
+        id: file.split(".")[0],
+        ...fileContents,
+      };
+      const rank = tierEntry.rank;
+      tiers.ranks[rank].push({ ...tierEntry });
+    }
+  }
+
+  return tiers;
+}
+
+async function getTierDirectories(path) {
+  return await readdir(path);
+}
+
+async function getTiers() {
+  const tiersDirs = await getTierDirectories(tiersPathFromWorkspaceRoot);
+  const allTiers = [];
+  for (const dir of tiersDirs) {
+    const tiers = await createTiersFromDir(dir);
+    allTiers.push(tiers);
+  }
+
+  return allTiers;
+}
+
+export default async function () {
+  return await getTiers();
+}
---
+++ tiers/chris-nolan-movies/memento.js
@@ -0,0 +1,9 @@
+export default {
+  rank: "S",
+  title: "Memento",
+  text: `
+  I saw this movie multiple times during its theatrical run in 2000. Prior to _Memento_ it was all the usual stuff, Michael Bay and whatever Will Smith was doing and whatever else you were supposed to watch as a movie-going teenager. But then, though I didn't know it at the time, _Memento_'s puzzle box of immaculate, clever filmmaking and storytelling was probably my gateway into smart arthouse movies. 
+  
+  All that and it's still a heck of a yarn to follow, with a cast of characters whose psyches balance on the edge of a ravine. Nolan's movies are all technically very competent, but only the best ones includes quality characterizations. _Memento_ has it all.
+  `,
+};
